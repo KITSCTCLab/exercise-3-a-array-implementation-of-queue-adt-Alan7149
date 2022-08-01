363f9bcde309150f6634a858594050fc2944a1ab
Alan Babu <97074137+Alan7149@users.noreply.github.com>
2022-08-01 15:30:52 +0530

    4th commit

--- Main.py
+++ Main.py
@@ -131,9 +131,7 @@ solution = Solution(length_of_text)
 
 # push/enqueue all the characters of string text to stack
 for index in range(length_of_text):
-    # Write code here
-    solution.push_character(text[index])
-    solution.enqueue_character(text[index])
+    self.stack.append(text[index])
 
 is_palindrome = True
 '''
@@ -143,11 +141,8 @@ compare both characters
 If the comparison fails, set is_palindrome as False.
 '''
 # Write the necessary logic
-for index in range(length_of_text):
-    if  solution.pop_character() != solution.dequeue_character():
-        is_palindrome = False
-        break
-
+if Solution.pop_character()!=Solution.dequeue_character():
+    is_palindrome = False
 
 # finally print whether string text is palindrome or not.
 if is_palindrome:
